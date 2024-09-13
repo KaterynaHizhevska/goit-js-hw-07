5ca274abe356c610d17a71ef1a4f5b896c207030
Kateryna <katagizevska@gmail.com>
2024-09-14 00:33:25 +0200

    add all

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let isFormValid = true;
+    const formData = {};
+
+    for (const element of form.elements) {
+        if (element.type !== 'submit') {
+            const value = element.value.trim();
+             if (value === '') {
+        isFormValid = false;
+             } else {
+                 formData[element.name] = value;
+      }
+    }
+    }
+    if (!isFormValid) {
+    alert('All form fields must be filled in');
+    } else {
+        console.log(formData);
+        form.reset();
+  }
+});
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,45 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById('boxes');
+  const boxSizeStep = 10;
+  const initialSize = 30;
+
+  boxesContainer.innerHTML = '';
+
+  const boxesFragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    const size = initialSize + i * boxSizeStep;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.margin = '5px';
+    boxesFragment.appendChild(box);
+  }
+
+  boxesContainer.appendChild(boxesFragment);
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById('boxes');
+  boxesContainer.innerHTML = '';
+}
+
+const input = document.querySelector('#controls input');
+const createButton = document.querySelector('button[data-create]');
+const destroyButton = document.querySelector('button[data-destroy]');
+
+createButton.addEventListener('click', () => {
+  const amount = Number(input.value);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  }
+  input.value = '';
+});
+destroyButton.addEventListener('click', () => {
+  destroyBoxes();
+});
